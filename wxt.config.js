f96a03b438106e1a6025e23d98581d0a875b8830
Sebastian Landwehr <info@sebastianlandwehr.com>
2024-03-05 02:30:41 +0100

    feat: init

--- wxt.config.js
+++ wxt.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'wxt';
-import vue from '@vitejs/plugin-vue';
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'wxt'
 
 export default defineConfig({
   manifest: {
     action: {},
     permissions: ['storage'],
   },
+  vite: () => ({
+    plugins: [vue()],
+  }),
   zip: {
     artifactTemplate: '{{browser}}.zip',
     sourcesTemplate: '{{browser}}-sources.zip',
   },
-  vite: () => ({
-    plugins: [vue()],
-  }),
-});
+})
